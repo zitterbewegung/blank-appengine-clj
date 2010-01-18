eedc43e3fa207c246bc2e1e3d04f5d1bae446f3d
Joshua Jay Herman <zitterbewegung@gmail.com>
2010-01-18 11:10:48 -0600

    Removed remnants of project.clj

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject replace-me "1.0.0"
+  :description "A very simple Fahrenheit to Celsius converter."
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+		[lein-gae "1.0.0-SNAPSHOT"]
+		[org.clojars.dvgb/compojure "0.3.1"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
