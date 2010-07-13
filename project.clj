c53617e82417578172cd2ddbe20db9020dd63076
Joshua Herman <jherman@aconsapart.com>
2010-07-12 22:24:44 -0500

    Changed to use lein

--- project.clj
+++ project.clj
@@ -1,26 +1,16 @@
-(defproject
- org.clojars.blank-appengine-clj
- "1.0.0"
- :compile-path
- "war/WEB-INF/classes"
- :dependencies
- [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
-  [lein-gae "1.0.0-SNAPSHOT"]
-  [org.clojars.dvgb/compojure "0.3.1"]
-  [org.clojars.hiredman/compojure "0.3.1"]
-  [com.google.appengine/appengine-api-1.0-sdk "1.2.1"]
-  [com.google.appengine/appengine-tools-sdk "1.2.1"]
-  [geronimo-spec/geronimo-spec-servlet "2.4-rc4"]
-  [org.clojars.hiredman/compojure "0.3.1"]
-  [com.google.appengine/appengine-api-1.0-sdk "1.2.1"]
-  [com.google.appengine/appengine-tools-sdk "1.2.1"]
-  [geronimo-spec/geronimo-spec-servlet "2.4-rc4"]]
- :dev-dependencies
- [[lein-clojars "0.5.0-SNAPSHOT"]]
- :appengine
- {:id "org.clojars.blank-appengine-clj",
-  :display-name "org.clojars.blank-appengine-clj",
-  :version 1,
-  :devport 8080}
- :description
- "A very simple starting off point for appengine-clj.")
+(defproject aconsapart "0-3v"
+  :description "Main site for aconsapart"
+  :namespaces [aconsapart.core]
+  :dev-dependencies [[swank-clojure "1.2.1"]]
+  :dependencies [[compojure "0.4.0"]
+                 [ring/ring-servlet "0.2.1"]
+                 [hiccup "0.2.4"]
+                 [appengine "0.2"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
+                 [com.google.appengine/appengine-api-labs "1.3.4"]] 
+  :dev-dependencies [[swank-clojure "1.2.1"]
+                     [ring/ring-jetty-adapter "0.2.0"]
+                     [com.google.appengine/appengine-local-runtime "1.3.4"]
+                    [com.google.appengine/appengine-api-stubs "1.3.4"]]
+  :compile-path "war/WEB-INF/classes"
+  :library-path "war/WEB-INF/lib")
