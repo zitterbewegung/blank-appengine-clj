f0140189f36a5a7b76775453b936340ce34d6db5
Joshua Herman <jherman@aconsapart.com>
2010-07-13 00:37:40 -0500

    Added install.sh;

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#Specify install as a keyword argument
+if [ $# -lt 1] ; then
+    echo -e "Please provide the root directory of your app engine installation to continue"
+    exit 1
+fi
+
+mvn install:install-file -DgroupId=com.google.appengine \
+-DartifactId=appengine-api-labs -Dversion=1.3.5 -Dpackaging=jar \
+-Dfile=$1/lib/user/appengine-api-labs-1.3.5.jar
+
+mvn install:install-file -DgroupId=com.google.appengine \
+-DartifactId=appengine-api-labs -Dversion=1.3.5 -Dpackaging=jar \
+-Dfile=$1/lib/user/appengine-api-1.0-sdk-1.3.5.jar
+
+mvn install:install-file -DgroupId=com.google.appengine \
+-DartifactId=appengine-api-labs -Dversion=1.3.5 -Dpackaging=jar \
+-Dfile=$1/lib/user/appengine-api-1.0-sdk-1.3.5.jar
+
+mvn install:install-file -DgroupId=com.google.appengine \
+-DartifactId=appengine-api-labs -Dversion=1.3.5 -Dpackaging=jar \
+-Dfile=$1/lib/impl/appengine-api-stubs-sdk-1.3.5.jar
+
+mvn install:install-file -DgroupId=com.google.appengine \
+-DartifactId=appengine-api-labs -Dversion=1.3.5 -Dpackaging=jar \
+-Dfile=$1/lib/impl/appengine-local-runtime.jar
